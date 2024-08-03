4e7586fe9635d5f38f6e9620eb055b161e450452
Saba Gul <gul@ualberta.ca>
2024-08-03 13:09:55 -0600

    feat: initial setup

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+import React from "react";
+
+export type INewUser = {
+    name: string;
+    email: string;
+    username: string;
+    password: string;
+};
+
+export type IUser = {
+    id: string;
+    name: string;
+    username: string;
+    email: string;
+    imageUrl: string;
+    bio: string;
+};
+
+export type IContextType = {
+    user: IUser;
+    isLoading: boolean;
+    setUser: React.Dispatch<React.SetStateAction<IUser>>;
+    isAuthenticated: boolean;
+    setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+    checkAuthUser: () => Promise<boolean>;
+};
