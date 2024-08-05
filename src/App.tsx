a31e46548cdf3ef6f7ea68d696e3ff1ea9e9e168
Saba Gul <gul@ualberta.ca>
2024-08-05 12:52:01 -0600

    chore: clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import './globals.css';
 import SigninForm from './_auth/forms/SigninForm';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import { Toaster } from "@/components/ui/toaster"
-import Explore from './_root/pages/Explore';
-import Saved from './_root/pages/Saved';
-import AllUsers from './_root/pages/AllUsers';
-import CreatePost from './_root/pages/CreatePost';
-import EditPost from './_root/pages/EditPost';
-import PostDetails from './_root/pages/PostDetails';
-import Profile from './_root/pages/Profile';
-import UpdateProfile from './_root/pages/UpdateProfile';
+
 
 const App = () => {
   return (
@@ -27,15 +20,15 @@ const App = () => {
             
             {/* private routes */}
             <Route element={<RootLayout />}>
-                <Route index element={<Home />}/>
-                <Route path='/explore' element={<Explore />} />
-                <Route path='/saved' element={<Saved />} />
-                <Route path='/all-users' element={<AllUsers />} />
-                <Route path='/create-post' element={<CreatePost />} />
-                <Route path='/update-post/:id' element={<EditPost />} />
-                <Route path='/posts/:id' element={<PostDetails />} />
-                <Route path='/profile/:id/*' element={<Profile />} />
-                <Route path='/update-profile/:id' element={<UpdateProfile />} />
+              <Route path='/all-users' element={<AllUsers />} />
+              <Route path='/create-post' element={<CreatePost />} />
+              <Route path='/update-post/:id' element={<EditPost />} />
+              <Route path='/explore' element={<Explore />} />
+              <Route index element={<Home />}/>
+              <Route path='/posts/:id' element={<PostDetails />} />
+              <Route path='/profile/:id/*' element={<Profile />} />
+              <Route path='/saved' element={<Saved />} />
+              <Route path='/update-profile/:id' element={<UpdateProfile />} />
             </Route>          
         </Routes>
 
