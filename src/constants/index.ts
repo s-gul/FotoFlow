ea8720e58b4d0b31380c738bc4e9abdf55e4f8c7
Saba Gul <gul@ualberta.ca>
2024-08-11 13:02:04 -0600

    chore: bottom bar completed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,26 @@
+export const bottombarLinks = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/assets/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Create",
+  },
+]
+
 export const sidebarLinks = [
   {
     imgURL: "/assets/icons/home.svg",
