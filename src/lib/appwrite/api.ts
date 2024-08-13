05c9bd5ec0f27e51816736bf1ac89ad1c7ca9d0c
Saba Gul <gul@ualberta.ca>
2024-08-13 13:36:17 -0600

    chore: completed home page

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -83,6 +83,23 @@ export async function deleteFile(fileId:string) {
     }
 }
 
+export async function deleteSavedPost(savedRecordId:string) {
+    try{
+        console.log(savedRecordId);
+        const statusCode = await databases.deleteDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.savesCollectionID,
+            savedRecordId,
+        )
+
+        if (!statusCode) throw Error;
+
+        return { status: 'ok' };
+    }catch (error){
+        console.log(error);
+    }
+}
+
 export async function getCurrentUser() {
     try {
         const currentAccount = await account.get();
@@ -117,6 +134,57 @@ export async function getFilePreview(fileId: string){
         if (!fileUrl) throw Error;
 
         return fileUrl;
+    }catch (error){
+        console.log(error);
+    }
+}
+
+export async function getRecentPosts() {
+    const posts = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        [Query.orderDesc('$createdAt'), Query.limit(20)]
+    )
+
+    if (!posts) throw Error;
+
+    return posts;
+}
+
+export async function likePost(postId:string, likesArray: string[]) {
+    try{
+        const updatedPost = await databases.updateDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            postId,
+            {
+                likes: likesArray
+            }
+        )
+
+        if (!updatedPost) throw Error;
+
+        return updatedPost;
+    }catch (error){
+        console.log(error);
+    }
+}
+
+export async function savePost(postId:string, userId: string) {
+    try{
+        const updatedPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.savesCollectionID,
+            ID.unique(),
+            {
+                user: userId,
+                post: postId,
+            }
+        )
+
+        if (!updatedPost) throw Error;
+
+        return updatedPost;
     }catch (error){
         console.log(error);
     }
